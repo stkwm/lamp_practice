3e4e7a4826bc6faa5bbd40ae9fb74c78f61f4e76
Kiwamu <kkiwamu2456ssato@gmail.com>
2020-08-12 00:43:23 +0900

    #13

--- www/model/history.php
+++ www/model/history.php
@@ -3,49 +3,73 @@ require_once MODEL_PATH . 'functions.php';
 require_once MODEL_PATH . 'db.php';
 
 // ユーザーごとの履歴テーブルの読み込み（ユーザー用）
+function get_history($db, $user_id) {
+  $sql = "
+    SELECT 
+      history.history_id,
+      created,
+      SUM(price * amount) AS total_price
+    FROM
+      history
+    INNER JOIN
+      history_details
+    ON
+      history.history_id = history_details.history_id
+    WHERE
+      user_id = ?
+    GROUP BY
+      history.history_id
+    ORDER BY
+      created
+    DESC
+    ";
+    $params = array($user_id);
+    return fetch_all_query($db, $sql, $params);
+}
 
-// 全ユーザの履歴テーブルの読み込み
+// すべての履歴テーブルの読み込み（ユーザー用）
+function get_all_history($db, $user_id) {
+    $sql = "
+      SELECT 
+        history.history_id,
+        created,
+        SUM(price * amount) AS total_price
+      FROM
+        history
+      INNER JOIN
+        history_details
+      ON
+        history.history_id = history_details.history_id
+      GROUP BY
+        history.history_id
+      ORDER BY
+        created
+      DESC
+      ";
+      return fetch_all_query($db, $sql);
+  }
 
 
-// // ユーザーごとの履歴詳細テーブルの読み込み（ユーザー用）
-// function get_history_details($db, $user_id) {
-//   $sql = "
-//     SELECT
-//       history_details.history_id
-//       history_details.price
-//       history_details.amount
-//       items.name
-//     FROM
-//       history_details
-//     INNER JOIN
-//       items
-//     ON
-//       history_details.item_id = items_id
-//     WHERE
-//       user_id = ?
-//     ";
-//   $params = array($user_id);
-//   return fetch_all_query($db, $sql, $params);
-// }
-
-// // 全ユーザーの履歴詳細テーブルの読み込み（管理者用）
-// function get_history_details($db, $user_id) {
-//   $sql = "
-//     SELECT
-//       history_details.history_id
-//       history_details.price
-//       history_details.amount
-//       items.name
-//     FROM
-//       history_details
-//     INNER JOIN
-//       items
-//     ON
-//       history_details.item_id = items_id
-//     ";
-//   $params = array($user_id);
-//   return fetch_all_query($db, $sql, $params);
-// }
+// 履歴詳細テーブルの読み込み
+function get_history_details($db, $history_id) {
+  $sql = "
+    SELECT
+      history_details.history_id
+      history_details.price
+      history_details.amount
+      items.name
+    FROM
+      history_details
+    INNER JOIN
+      items
+    ON
+      history_details.item_id = items.item_id
+    WHERE
+      history_id = ?
+    ";
+  $params = array($history_id);
+  return fetch_all_query($db, $sql, $params);
+}
 
 // 履歴テーブルにデータを挿入する関数
 // phpmyadminで日時のデフォルト値をCURRENT_STAMPに設定してある→$sqlに現在日時の設定必要ない
